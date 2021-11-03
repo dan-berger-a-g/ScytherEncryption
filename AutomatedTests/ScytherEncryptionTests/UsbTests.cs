4dab51aecf970e96b3d18d6c9fd9aa6bfb55be7f
dan-berger-a-g <92645390+dan-berger-a-g@users.noreply.github.com>
2021-11-03 15:31:24 -0400

    Add files via upload

---
+++ AutomatedTests/ScytherEncryptionTests/UsbTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ScytherEncryption
+{
+    [TestFixture]
+    class UsbTests
+    {
+        Usb o;
+        string tempFile;
+        string tempFile1;
+
+        [SetUp]
+        public void SetUp()
+        {
+            o = new Usb();
+            tempFile = Path.GetTempFileName();
+            tempFile1 = Path.GetTempFileName();
+        }
+
+            [TearDown]
+            public void TearDown()
+            {
+                File.Delete(tempFile);
+                File.Delete(tempFile1);
+            }
+
+            [Test]
+            public void TestRightUsbStickandCombining()
+            {
+                bool written = false;
+                var drive = DriveInfo.GetDrives().FirstOrDefault(x => x.IsReady);
+                string driveRoot = drive.RootDirectory.FullName;
+                string file = Path.Combine(driveRoot, tempFile1);
+                if (file != null)
+                {
+                    written = true;
+                }
+            Assert.IsTrue(written);
+            Assert.AreNotEqual(driveRoot, null);
+            }
+         //[Test]
+         /*public void ComineFiles()
+        {
+            bool written = false;
+            var drive = DriveInfo.GetDrives().FirstOrDefault(x => x.IsReady);
+            string driveRoot = drive.RootDirectory.FullName;
+            Directory.CreateDirectory(tempFile1);
+            string file = Path.Combine(driveRoot, tempFile1);
+            if(file != null)
+            {
+                written = true;
+            }
+            Assert.Pass();*/
+        }
+    }
+
